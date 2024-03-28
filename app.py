cf32f956214ad9801661265d9d236587178ea684
Abigail de Joode <contact@abbydiode.com>
2024-03-28 21:07:27 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import json
 import pygame
 from datetime import datetime
 from dualshock4_button import DualShock4Button
@@ -10,20 +11,16 @@ inputs = []
 def handle_input(button, state):
     print(f'{button.name} {state.name}')
     inputs.append(Input(button, datetime.now() - start_time, state))
-    print(inputs[-1].__dict__)
 
 running = True
 
 pygame.init()
 
-pygame.display.set_mode((1280, 720))
-pygame.display.set_caption('DirectInput Serializer')
+pygame.joystick.init()
 
 pygame.mixer.init()
 pygame.mixer.music.load('karaoke.mp3')
 pygame.mixer.music.play()
-
-pygame.joystick.init()
 
 joystick = pygame.joystick.Joystick(0)
 print(f'{joystick.get_name()} Connected')
@@ -33,12 +30,18 @@ previous_values = {i: 0 for i in range(joystick.get_numbuttons())}
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
+            with open(f'inputs-{datetime.now()}.json', 'w') as file:
+                file.write(json.dumps([input.__dict__ for input in inputs]))
             running = False
+
 
     for i in range(joystick.get_numbuttons()):
         value = joystick.get_button(i)
         if value != previous_values[i]:
-            handle_input(DualShock4Button(i), ButtonState(value))
+            try:
+                handle_input(DualShock4Button(i), ButtonState(value))
+            except ValueError:
+                continue
             previous_values[i] = value
 
 pygame.quit()
