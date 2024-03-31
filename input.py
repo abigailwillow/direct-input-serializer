96bc1b54e640e93bccfd213a829390e3480ec233
Abigail de Joode <contact@abbydiode.com>
2024-03-31 20:16:14 +0200

    Delete input.py

--- input.py
+++
@@ -1,9 +0,0 @@
-from dualshock4_button import DualShock4Button
-from button_state import ButtonState
-
-class Input:
-    def __init__(self, button: DualShock4Button, time: int, state: ButtonState, rapid: bool = False):
-        self.button = button.name
-        self.time = time
-        self.state = state.name
-        self.rapid = rapid
