6d73da3935abcc695d6e98fcb5197bc4011ac639
Abigail de Joode <contact@abbydiode.com>
2024-04-03 21:38:54 +0200

    Map dualshock 4 buttons to strings

--- models/dualshock4_button.py
+++ models/dualshock4_button.py
@@ -1,7 +1,7 @@
 from enum import Enum
 
 class DualShock4Button(Enum):
-    CROSS = 0
-    CIRCLE = 1
-    SQUARE = 2
-    TRIANGLE = 3
+    CROSS = 'CROSS'
+    CIRCLE = 'CIRCLE'
+    SQUARE = 'SQUARE'
+    TRIANGLE = 'TRIANGLE'
